e701202418fd37b92b480c0999ec70edcc5a6f84
thinhwar123 <52587916+thinhwar123@users.noreply.github.com>
2023-07-31 14:58:35 +0700

    Update Facade

---
+++ Packages/com.twbase.twdesignpattern/Runtime/Facade/DataSaveSystem/DataGlobal.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TW.DesignPattern
+{
+    public class DataGlobal : Singleton<DataGlobal>
+    {
+        private IDataAccess m_DataAccess;
+        public IDataAccess DataAccess => m_DataAccess ??= new ObscuredPrefsDataAccess();
+
+        public T GetData<T>(string dataKey, T defaultValue = default)
+        {
+            return DataAccess.GetData<T>(dataKey, defaultValue);
+        }
+        public void SetData<T>(string dataKey, T value)
+        {
+            DataAccess.SetData<T>(dataKey, value);
+        }
+    } 
+}
---
+++ Packages/com.twbase.twdesignpattern/Runtime/Facade/DataSaveSystem/ObscuredPrefsDataAccess.cs
@@ -0,0 +1,17 @@
+using CodeStage.AntiCheat.Storage;
+
+namespace TW.DesignPattern
+{
+    public class ObscuredPrefsDataAccess : IDataAccess
+    {
+        public T GetData<T>(string dataKey, T defaultValue = default)
+        {
+            return ObscuredPrefs.Get<T>(dataKey, defaultValue);
+        }
+
+        public void SetData<T>(string dataKey, T value)
+        {
+            ObscuredPrefs.Set<T>(dataKey, value);
+        }
+    } 
+}
