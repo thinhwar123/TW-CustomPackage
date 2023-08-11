9370ec8b1ba312fbe247454b9cd39d938a650040
thinhwar123 <52587916+thinhwar123@users.noreply.github.com>
2023-08-11 11:25:58 +0700

    Update Unity Analytics

--- Packages/com.tw.twanalytics/Runtime/UnityAnalytics/UnityAnalytics.cs
+++ Packages/com.tw.twanalytics/Runtime/UnityAnalytics/UnityAnalytics.cs
@@ -1,62 +1,61 @@
-using System;
 using System.Threading.Tasks;
-using TW.DesignPattern;
 using Unity.Services.Analytics;
 using System.Collections.Generic;
 using Unity.Services.Core;
 using UnityEngine;
 using UnityEngine.Events;
 
-
-public class UnityAnalytics : MonoBehaviour
+namespace TW.Analytics
 {
-    public static UnityAnalytics Instance { get; private set; }
-
-    private static readonly Queue<UnityAction> ExecutionEvent = new Queue<UnityAction>();
-    private static bool isInitialized = false;
-
-    private void Awake()
+    public class UnityAnalytics : MonoBehaviour
     {
-        if (Instance == null)
+        public static UnityAnalytics Instance { get; private set; }
+
+        private static readonly Queue<UnityAction> ExecutionEvent = new Queue<UnityAction>();
+        private static bool isInitialized = false;
+
+        private void Awake()
         {
-            Instance = this;
-            DontDestroyOnLoad(this.gameObject);
-        }
-    }
-
-    private async void Start()
-    {
-        await InitService();
-    }
-    private void Update()
-    {
-        if (!isInitialized) return;
-        lock (ExecutionEvent)
-        {
-            while (ExecutionEvent.Count > 0)
+            if (Instance == null)
             {
-                ExecutionEvent.Dequeue()?.Invoke();
+                Instance = this;
+                DontDestroyOnLoad(this.gameObject);
+            }
+        }
+
+        private async void Start()
+        {
+            await InitService();
+        }
+        private void Update()
+        {
+            if (!isInitialized) return;
+            lock (ExecutionEvent)
+            {
+                while (ExecutionEvent.Count > 0)
+                {
+                    ExecutionEvent.Dequeue()?.Invoke();
+                }
+            }
+        }
+        private async Task InitService()
+        {
+            await UnityServices.InitializeAsync();
+            Debug.Log("UnityServices Initialize: Complete");
+            AnalyticsService.Instance.StartDataCollection();
+            Debug.Log("UnityServices ConsentGiven: Complete");
+            isInitialized = true;
+        }
+        public static void SendCustomEvent(string eventName, IDictionary<string, object> eventParams)
+        {
+            lock (ExecutionEvent)
+            {
+                ExecutionEvent.Enqueue(() =>
+                {
+                    AnalyticsService.Instance.CustomData(eventName, eventParams);
+                });
             }
         }
     }
-    private async Task InitService()
-    {
-        await UnityServices.InitializeAsync();
-        Debug.Log("UnityServices Initialize: Complete");
-        AnalyticsService.Instance.StartDataCollection();
-        Debug.Log("UnityServices ConsentGiven: Complete");
-        isInitialized = true;
-    }
-    public static void SendCustomEvent(string eventName, IDictionary<string, object> eventParams)
-    {
-        lock (ExecutionEvent)
-        {
-            ExecutionEvent.Enqueue(() =>
-            {
-                AnalyticsService.Instance.CustomData(eventName, eventParams);
-            });
-        }
-    }
-
 
 }
