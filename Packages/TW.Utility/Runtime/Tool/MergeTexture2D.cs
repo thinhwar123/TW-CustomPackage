44cbf6336307e08db2fc51110c9259eb848bee10
thinhwar123 <52587916+thinhwar123@users.noreply.github.com>
2023-09-26 18:00:10 +0700

    Update MergeTexture2D.cs

--- Packages/TW.Utility/Runtime/Tool/MergeTexture2D.cs
+++ Packages/TW.Utility/Runtime/Tool/MergeTexture2D.cs
@@ -1,11 +1,15 @@
 using System.IO;
 using System.Linq;
 using Sirenix.Utilities;
-using UnityEditor;
 using UnityEngine;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 namespace TW.Utility.Tool
 {
+#if UNITY_EDITOR
     public class MergeTexture2D : Editor
     {
         private static Texture2D[] Texture2Ds { get; set; }
@@ -91,5 +95,6 @@ namespace TW.Utility.Tool
         }
 
     }
+#endif
 
 }
