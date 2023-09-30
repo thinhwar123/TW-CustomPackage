10bf101b048af45b4715db804f0d068254112350
thinhwar123 <52587916+thinhwar123@users.noreply.github.com>
2023-09-30 18:37:40 +0700

    Update BigNumberEditorAttribute.cs

--- Packages/TW.Utility/Runtime/CustomType/BigNumberEditorAttribute.cs
+++ Packages/TW.Utility/Runtime/CustomType/BigNumberEditorAttribute.cs
@@ -1,9 +1,9 @@
 using System.Collections.Generic;
 using System.Linq;
-using UnityEditor;
 using UnityEngine;
 using System.Text.RegularExpressions;
 #if UNITY_EDITOR
+using UnityEditor;
 using Sirenix.OdinInspector.Editor;
 #endif
 
