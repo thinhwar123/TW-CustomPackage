ee89225bde3d95b03868c45ea03e378b53644b59
thinhwar123 <52587916+thinhwar123@users.noreply.github.com>
2023-11-13 08:45:06 +0700

    Update ABakingSheet

--- Packages/TW.Utility/Runtime/Extension/ABakingSheet.cs
+++ Packages/TW.Utility/Runtime/Extension/ABakingSheet.cs
@@ -3,20 +3,38 @@ using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
-using Newtonsoft.Json;
+using Unity.Plastic.Newtonsoft.Json;
 using UnityEngine;
 using UnityEngine.Networking;
 
 namespace TW.Utility.Extension
 {
+    /// <summary>
+    /// A utility class for retrieving the content of a Google spreadsheet in CSV or Json format.
+    /// For example, to retrieve the content of the "Sheet1" tab in https://docs.google.com/spreadsheets/d/16WoJtP7SzaI9F8B_6MV0vIF57I8M7W3-DhL4mKVAF0Y/edit#gid=0
+    /// spreadsheetId = 16WoJtP7SzaI9F8B_6MV0vIF57I8M7W3-DhL4mKVAF0Y
+    /// tabName = Sheet1
+    /// </summary>
     public static class ABakingSheet
     {
+        /// <summary>
+        /// Retrieves the content of a spreadsheet specified by its ID and tab name in CSV format.
+        /// </summary>
+        /// <param name="spreadsheetId">The ID of the target spreadsheet.</param>
+        /// <param name="tabName">The name of the tab (worksheet) within the spreadsheet.</param>
+        /// <returns>A Task representing the asynchronous operation, returning the spreadsheet content in CSV format.</returns>
         public static async Task<string> GetCsv(string spreadsheetId, string tabName)
         {
             string json = await SendRequest(spreadsheetId, tabName);
             return JsonToCsv(json);
         }
-
+        
+        /// <summary>
+        /// Retrieves the content of a spreadsheet specified by its ID and tab name in JSON format.
+        /// </summary>
+        /// <param name="spreadsheetId">The ID of the target spreadsheet.</param>
+        /// <param name="tabName">The name of the tab (worksheet) within the spreadsheet.</param>
+        /// <returns>A Task representing the asynchronous operation, returning the spreadsheet content in JSON format.</returns>
         public static async Task<string> GetJson(string spreadsheetId, string tabName)
         {
             return await SendRequest(spreadsheetId, tabName);
@@ -25,14 +43,13 @@ namespace TW.Utility.Extension
         private static async Task<string> SendRequest(string spreadsheetId, string tabName)
         {
             string url = $"https://opensheet.elk.sh/{spreadsheetId}/{tabName}";
-            Debug.Log(url);
             UnityWebRequest www = UnityWebRequest.Get(url);
 
             await www.SendWebRequest();
 
             if (www.result == UnityWebRequest.Result.Success)
             {
-                Debug.Log("Success");
+                Debug.Log($"Update Success {spreadsheetId}/{tabName}");
                 return www.downloadHandler.text;
             }
             else
