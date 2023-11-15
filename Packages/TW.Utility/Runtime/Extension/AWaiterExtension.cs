24d004076072f6f8e9e066c30c0f40d3f71bac14
thinhwar123 <52587916+thinhwar123@users.noreply.github.com>
2023-11-15 16:03:18 +0700

    Update AWaiterExtension.cs

--- Packages/TW.Utility/Runtime/Extension/AWaiterExtension.cs
+++ Packages/TW.Utility/Runtime/Extension/AWaiterExtension.cs
@@ -7,11 +7,8 @@ namespace TW.Utility.Extension
 	{
         public static AWaiter DependOn(this AWaiter currentAWaiter, AWaiter dependAWaiter)
         {
-            currentAWaiter.OwnTween.Pause();
-            dependAWaiter.OwnTween.onComplete += () =>
-            {
-                currentAWaiter.OwnTween.Restart();
-            };
+            currentAWaiter.Pause();
+            dependAWaiter.OnComplete(currentAWaiter.Restart);
             return currentAWaiter;
         }
         public static AWaiter DependOn(this Tween currentSWaiter, AWaiter dependAWaiter)
@@ -19,61 +16,72 @@ namespace TW.Utility.Extension
             AWaiter aWaiter = currentSWaiter;
             return aWaiter.DependOn(dependAWaiter);
         }
-        public static AWaiter DelayCall(this Action action, float time)
-        {
-            float process = 0; 
-            
-            return DOTween.To(() => process, value => process = value, 1, time).OnComplete(() => action?.Invoke());
-        }
-
-        public static AWaiter DelayCall(this AWaiter aWaiter, float time)
-        {
-            float process = 0;
-            aWaiter.OwnTween = DOTween.To(() => process, value => process = value, 1, time).OnComplete(() => aWaiter.OnTweenCompleteCallback?.Invoke());
-            return aWaiter;
-        }
     }
 
     public class AWaiter
     {
+        public float Process { get; private set; }
+        public float Delay { get; private set; }
         public bool IsComplete { get; private set; }
         public Tween OwnTween { get; set; }
-        public Action OnTweenCompleteCallback { get; set; }
-
+        private Action OnTweenStartCallback { get; set; }
+        private Action OnTweenCompleteCallback { get; set; }
         public AWaiter()
         {
-            IsComplete = false;
-            OnTweenCompleteCallback = null;
+            IsComplete = true;
+            OnTweenStartCallback += () => IsComplete = false;
+            OnTweenCompleteCallback += () => IsComplete = true;
         }
-
-        public AWaiter(Action onCompleteAction)
+        public AWaiter(Action onStartAction, Action onCompleteAction) : this()
         {
-            IsComplete = false;
-            OnTweenCompleteCallback = onCompleteAction;
+            OnTweenStartCallback += onStartAction;
+            OnTweenCompleteCallback += onCompleteAction;
         }
-        public AWaiter(Tween tween)
+        public AWaiter(Tween tween) : this(() => tween.onPlay?.Invoke(), () => tween.onComplete?.Invoke())
         {
-            IsComplete = false;
-
             OwnTween = tween;
-            OwnTween.onComplete += (() =>
-            {
-                IsComplete = true;
-                OnTweenCompleteCallback?.Invoke();
-            });
-
+            OwnTween.OnUpdate(() => this.Process = OwnTween.Elapsed() / OwnTween.Duration())
+                .OnStart(() => OnTweenStartCallback?.Invoke())
+                .OnComplete(() => OnTweenCompleteCallback?.Invoke());
         }
         public static implicit operator AWaiter(Tween tween)
         {
             return new AWaiter(tween);
         }
-
-        public AWaiter OnComplete(Action action)
+        public void Play()
         {
-            OnTweenCompleteCallback = action;
-            return this;
+            Process = 0;
+            if (OwnTween == null)
+            {
+                OwnTween = DOTween.To(() => Process, value => Process = value, 1, Delay)
+                    .OnStart(() => OnTweenStartCallback?.Invoke())
+                    .OnComplete(() => OnTweenCompleteCallback?.Invoke());
+            }
+            else
+            {
+                OwnTween.Restart();
+            }
         }
-
+        public void Pause()
+        {
+            OwnTween?.Pause();
+        }
+        public void Restart()
+        {
+            OwnTween?.Restart();
+        }
+        public void SetDelay(float delay)
+        {
+            Delay = delay;
+        }
+        public void OnStart(Action onStartAction)
+        {
+            OnTweenStartCallback += onStartAction;
+        }
+        public void OnComplete(Action onCompleteAction)
+        {
+            OnTweenCompleteCallback += onCompleteAction;
+        }
         public void Kill()
         {
             OwnTween?.Kill();
