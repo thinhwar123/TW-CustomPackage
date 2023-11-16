c283d735eeb04f887fab99a994aa82a2a88e0e8b
thinhwar123 <52587916+thinhwar123@users.noreply.github.com>
2023-11-16 09:52:36 +0700

    Update ATimeSpanExtension.cs

--- Packages/TW.Utility/Runtime/Extension/ATimeSpanExtension.cs
+++ Packages/TW.Utility/Runtime/Extension/ATimeSpanExtension.cs
@@ -34,6 +34,21 @@ namespace TW.Utility.Extension
             if (timeSpan.Seconds > 0 || !string.IsNullOrWhiteSpace(sb.ToString())) sb.Append(string.IsNullOrWhiteSpace(sb.ToString()) ? $"{timeSpan.Seconds}" : $"{timeSpan.Seconds:00}");
             return sb.ToString();
         }
+        /// <summary>
+        /// This method extends the TimeSpan class and converts a given TimeSpan object to a human-readable string format that includes days, hours, minutes, and seconds.
+        /// </summary>
+        /// <param name="timeSpan">The TimeSpan object to convert to a readable string format.</param>
+        /// <returns></returns>
+        public static string ToStringFull(this TimeSpan timeSpan)
+        {
+            StringBuilder sb = new StringBuilder();
+            if (timeSpan.TotalSeconds < 1) return sb.ToString();
+            if (timeSpan.Days > 0) sb.Append($"{timeSpan.Days}d ");
+            if (timeSpan.Hours > 0 || !string.IsNullOrWhiteSpace(sb.ToString())) sb.Append(string.IsNullOrWhiteSpace(sb.ToString()) ? $"{timeSpan.Hours}h " : $"{timeSpan.Hours:00}h ");
+            if (timeSpan.Minutes > 0 || !string.IsNullOrWhiteSpace(sb.ToString())) sb.Append(string.IsNullOrWhiteSpace(sb.ToString()) ? $"{timeSpan.Minutes}m " : $"{timeSpan.Minutes:00}m ");
+            if (timeSpan.Seconds > 0 || !string.IsNullOrWhiteSpace(sb.ToString())) sb.Append(string.IsNullOrWhiteSpace(sb.ToString()) ? $"{timeSpan.Seconds}s" : $"{timeSpan.Seconds:00}s");
+            return sb.ToString();
+        }
     }
 
 }
