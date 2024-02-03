e4e1db5a1d991c73edf668379623200cdd5d9495
thinhwar123 <52587916+thinhwar123@users.noreply.github.com>
2024-02-03 08:56:07 +0700

    Update Singleton

--- Packages/TW.Utility/Runtime/DesignPattern/Singleton/DDOLSingleton.cs
+++ Packages/TW.Utility/Runtime/DesignPattern/Singleton/DDOLSingleton.cs
@@ -13,23 +13,18 @@ namespace TW.Utility.DesignPattern
         {
             get
             {
-                if (instance == null)
-                {
-                    // Find singleton
-                    instance = FindObjectOfType<T>();
+                if (instance != null) return instance;
+                // Find singleton
+                instance = FindObjectOfType<T>();
 
-                    // Create new instance if one doesn't already exist.
-                    if (instance == null)
-                    {
-                        // Need to create a new GameObject to attach the singleton to.
-                        GameObject singletonObject = new GameObject();
-                        instance = singletonObject.AddComponent<T>();
-                        singletonObject.name = typeof(T).ToString() + " (Singleton)";
-                        singletonObject.AddComponent<DontDestroyOnLoadMonoBehavior>();
-
-                    }
-
-                }
+                // Create new instance if one doesn't already exist.
+                if (instance != null) return instance;
+                
+                // Need to create a new GameObject to attach the singleton to.
+                GameObject singletonObject = new GameObject();
+                instance = singletonObject.AddComponent<T>();
+                singletonObject.name = typeof(T).ToString() + " (Singleton)";
+                singletonObject.AddComponent<DontDestroyOnLoadMonoBehavior>();
                 return instance;
             }
         }
--- Packages/TW.Utility/Runtime/DesignPattern/Singleton/Singleton.cs
+++ Packages/TW.Utility/Runtime/DesignPattern/Singleton/Singleton.cs
@@ -11,23 +11,26 @@ namespace TW.Utility.DesignPattern
         {
             get
             {
-                if (instance == null)
-                {
-                    // Find singleton
-                    instance = FindObjectOfType<T>();
+                if (instance != null) return instance;
+                // Find singleton
+                instance = FindObjectOfType<T>();
 
-                    // Create new instance if one doesn't already exist.
-                    if (instance == null)
-                    {
-                        // Need to create a new GameObject to attach the singleton to.
-                        GameObject singletonObject = new GameObject();
-                        instance = singletonObject.AddComponent<T>();
-                        singletonObject.name = typeof(T).ToString() + " (Singleton)";
-
-                    }
-
-                }
+                // Create new instance if one doesn't already exist.
+                if (instance != null) return instance;
+                
+                // Need to create a new GameObject to attach the singleton to.
+                GameObject singletonObject = new GameObject();
+                instance = singletonObject.AddComponent<T>();
+                singletonObject.name = typeof(T).ToString() + " (Singleton)";
                 return instance;
+            }
+        }
+        
+        protected virtual void Awake()
+        {
+            if (Instance != this)
+            {
+                Destroy(gameObject);
             }
         }
     }
