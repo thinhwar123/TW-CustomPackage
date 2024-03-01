2eba41258a68e604b185c4bbe46301f3b3d20512
thinhwar123 <52587916+thinhwar123@users.noreply.github.com>
2024-03-01 15:03:31 +0700

    Update AVisualElement.cs

--- Packages/TW.GUI/Runtime/AVisualElement/AVisualElement.cs
+++ Packages/TW.GUI/Runtime/AVisualElement/AVisualElement.cs
@@ -21,8 +21,6 @@ namespace TW.GUI
             Button,
             Rect,
         }
-        
-#if UNITY_EDITOR
 
         [field: SerializeField, HorizontalGroup("Action", 100), ToggleLeft, OnValueChanged(nameof(OnAutoApplyChanged))] 
         public bool AutoApply {get; private set;} = true;
@@ -38,17 +36,24 @@ namespace TW.GUI
         private CancellationTokenSource CancellationTokenSource { get; set; }
         private IEnumerable<ValueDropdownItem> GetPresetList()
         {
+#if UNITY_EDITOR
             return AVisualElementPresetGlobalConfig.Instance.GetPresets(VisualElementType)
                 ?.Select(preset => new ValueDropdownItem(preset.name, preset));
+#else
+            return null;
+#endif
         }
         [Button(SdfIconType.InfoCircle, ""), HorizontalGroup("MainProperty", 20)]
         private void SelectCurrentPreset()
         {
+#if UNITY_EDITOR
             if (Preset == null) return;
             EditorGUIUtility.PingObject(Preset);
+#endif
         }
         private void OnVisualElementTypeChanged()
         {
+#if UNITY_EDITOR
             AVisualElementPresetGlobalConfig.Instance.UpdateAllPreset();
             Preset = AVisualElementPresetGlobalConfig.Instance.GetPresets(VisualElementType)?[0];
             OverridePresetProperties.Init(VisualElementType, this, Preset);
@@ -56,59 +61,77 @@ namespace TW.GUI
             {
                 ApplyPreset();
             }
+#endif
         }
         private void OnPresetChanged()
         {
+#if UNITY_EDITOR
             OverridePresetProperties.Init(VisualElementType, this, Preset);
             if (AutoApply)
             {
                 ApplyPreset();
             }
+#endif
         }
         private void OnAutoApplyChanged()
         {
+#if UNITY_EDITOR
             OverridePresetProperties.Init(VisualElementType, this, Preset);
             if (AutoApply)
             {
                 ApplyPreset();
             }
+#endif
         }
 
         private bool HasOverridePresetProperties()
         {
+#if UNITY_EDITOR
             return OverridePresetProperties.PresetProperties.Count > 0;
+#else
+            return false;
+#endif
         }
 
         [OnInspectorInit]
         private void OnInspectorInit()
         {
+#if UNITY_EDITOR
             OverridePresetProperties.Init(VisualElementType, this, Preset);
             CancellationTokenSource = new CancellationTokenSource();
             EditorUtility.SetDirty(this);
             _ = UpdateProperties(CancellationTokenSource.Token);
+#endif
         }
         [OnInspectorDispose]
         private void OnInspectorDispose()
         {
+#if UNITY_EDITOR
             CancellationTokenSource?.Cancel();
             CancellationTokenSource?.Dispose();
             CancellationTokenSource = null;
+#endif
         }
         private async UniTask UpdateProperties(CancellationToken cancellationToken)
         {
+#if UNITY_EDITOR
             if (CancellationTokenSource.IsCancellationRequested) return;
             OverridePresetProperties.UpdateOverrideProperties();
             await UniTask.Delay(1000, cancellationToken: cancellationToken);
             _ = UpdateProperties(cancellationToken);
+#else
+            await UniTask.Yield();            
+#endif
         }
 
         [Button, ShowIf("@!AutoApply"), HorizontalGroup("Action")]
         public void ApplyPreset()
         {
+#if UNITY_EDITOR
             if (Preset == null) return;
             Preset.Apply(this);
-        }
 #endif
+        }
     }
 
 }
