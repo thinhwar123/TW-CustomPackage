6717e2cceee65c859142571ce34c1c0c7c8fc160
thinhwar123 <52587916+thinhwar123@users.noreply.github.com>
2024-04-12 17:19:50 +0700

    Update AnimationExtensions.cs

--- Packages/TW.UGUI/Runtime/UGUI/Utility/AnimationExtensions.cs
+++ Packages/TW.UGUI/Runtime/UGUI/Utility/AnimationExtensions.cs
@@ -10,35 +10,35 @@ namespace TW.UGUI.Utility
 {
     public static class AnimationExtensions
     {
-        private static Dictionary<IAnimation, AnimationPlayer> AnimationPlayers { get; set; } = new();
-        public static async UniTask PlayAsync(this IAnimation self, IProgress<float> progress = null)
-        {
-            if (AnimationPlayers.TryGetValue(self, out AnimationPlayer player))
-            {
-                player.CancellationTokenSource.Cancel();
-                player.CancellationTokenSource.Dispose();
-                
-                Pool<AnimationPlayer>.Shared.Return(player);
-                AnimationPlayers.Remove(self);
-            }
-            
-            player = Pool<AnimationPlayer>.Shared.Rent();
-            AnimationPlayers.Add(self, player);
-            player.Initialize(self, new CancellationTokenSource());
-
-            progress?.Report(0.0f);
-            player.Play();
-
-            while (player.IsFinished == false)
-            {
-                await UniTask.NextFrame(player.CancellationTokenSource.Token);
-                player.Update(Time.unscaledDeltaTime);
-                progress?.Report(player.Time / (self.Duration + self.Delay));
-            }
-
-            Pool<AnimationPlayer>.Shared.Return(player);
-            AnimationPlayers.Remove(self);
-        }
+        // private static Dictionary<IAnimation, AnimationPlayer> AnimationPlayers { get; set; } = new();
+        // public static async UniTask PlayAsync(this IAnimation self, IProgress<float> progress = null)
+        // {
+        //     if (AnimationPlayers.TryGetValue(self, out AnimationPlayer player))
+        //     {
+        //         player.CancellationTokenSource.Cancel();
+        //         player.CancellationTokenSource.Dispose();
+        //         
+        //         Pool<AnimationPlayer>.Shared.Return(player);
+        //         AnimationPlayers.Remove(self);
+        //     }
+        //     
+        //     player = Pool<AnimationPlayer>.Shared.Rent();
+        //     AnimationPlayers.Add(self, player);
+        //     player.Initialize(self, new CancellationTokenSource());
+        //
+        //     progress?.Report(0.0f);
+        //     player.Play();
+        //
+        //     while (player.IsFinished == false)
+        //     {
+        //         await UniTask.NextFrame(player.CancellationTokenSource.Token);
+        //         player.Update(Time.unscaledDeltaTime);
+        //         progress?.Report(player.Time / (self.Duration + self.Delay));
+        //     }
+        //
+        //     Pool<AnimationPlayer>.Shared.Return(player);
+        //     AnimationPlayers.Remove(self);
+        // }
         
         // public static async UniTask PlayAsync(this IAnimation self, IProgress<float> progress = null)
         // {
@@ -69,16 +69,16 @@ namespace TW.UGUI.Utility
         //     AnimationPlayers.Remove(self);
         // }
         
-        public static void Stop(this IAnimation self)
-        {
-            if (AnimationPlayers.TryGetValue(self, out AnimationPlayer player))
-            {
-                player.CancellationTokenSource.Cancel();
-                player.CancellationTokenSource.Dispose();
-                
-                Pool<AnimationPlayer>.Shared.Return(player);
-                AnimationPlayers.Remove(self);
-            }
-        }
+        // public static void Stop(this IAnimation self)
+        // {
+        //     if (AnimationPlayers.TryGetValue(self, out AnimationPlayer player))
+        //     {
+        //         player.CancellationTokenSource.Cancel();
+        //         player.CancellationTokenSource.Dispose();
+        //         
+        //         Pool<AnimationPlayer>.Shared.Return(player);
+        //         AnimationPlayers.Remove(self);
+        //     }
+        // }
     }
 }
