de4d082ea3cc284ecb8733abe0709260f8c72b4f
thinhwar123 <52587916+thinhwar123@users.noreply.github.com>
2024-06-10 17:25:45 +0700

    Update UGUI

---
+++ Packages/TW.UGUI/Runtime/UGUI/MVPPattern/IAPresenter.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Cysharp.Threading.Tasks;
+
+namespace TW.UGUI.MVPPattern
+{
+    public interface IAPresenter
+    {
+        UniTask Initialize(Memory<object> args);
+    }
+}
