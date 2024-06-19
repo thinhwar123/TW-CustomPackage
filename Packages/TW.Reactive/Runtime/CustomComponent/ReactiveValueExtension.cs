375dd4c597e53d16bd87b5de332714291289b734
thinhwar123 <52587916+thinhwar123@users.noreply.github.com>
2024-06-19 15:00:03 +0700

    Update ReactiveValueExtension

--- Packages/TW.Reactive/Runtime/CustomComponent/ReactiveValueExtension.cs
+++ Packages/TW.Reactive/Runtime/CustomComponent/ReactiveValueExtension.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using R3;
-using UnityEngine;
+using UnityEngine.UI;
 
 namespace TW.Reactive.CustomComponent
 {
@@ -11,6 +11,12 @@ namespace TW.Reactive.CustomComponent
             return reactiveValue1.ReactiveProperty.CombineLatest(reactiveValue2.ReactiveProperty, (t1, t2) => (t1, t2))
                 .Subscribe(onNext);
         }
-
+        public static IDisposable SetOnClickDestination(this Button self, Action<Unit> onClick)
+        {
+            return self.onClick
+                .AsObservable()
+                .Subscribe(onClick)
+                .AddTo(self);
+        }
     }
 }
