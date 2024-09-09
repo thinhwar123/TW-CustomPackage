766bb3bee912ef6bd5ee4d501ebf517a986f4b7a
thinh <thinhwar123@gmail.com>
2024-09-09 15:00:12 +0700

    Add AIntExtension

---
+++ Packages/TW.Utility/Runtime/Extension/AIntExtension.cs
@@ -0,0 +1,15 @@
+﻿namespace TW.Utility.Extension
+{
+    public static class AIntExtension
+    {
+        public static int Repeat(this int value, int length)
+        {
+            while (value >= length)
+            {
+                value -= length;
+            }
+
+            return value;
+        }
+    }
+}
