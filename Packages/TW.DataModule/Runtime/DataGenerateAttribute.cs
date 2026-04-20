211834204c13bf54b59cd437ae633c2b79168863
thinh <thinhwar123@gmail.com>
2026-04-20 08:53:21 +0700

    Add DataModule

---
+++ Packages/TW.DataModule/Runtime/DataGenerateAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace DataModule
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct,AllowMultiple = false, Inherited = true)]
+    public class DataGenerateAttribute : Attribute
+    {
+        public int Order { get; set; } = 0;
+
+        public DataGenerateAttribute(int order = 0)
+        {
+            Order = order;
+        }
+    }
+}
