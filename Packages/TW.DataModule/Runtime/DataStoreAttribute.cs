f262210719ce15b14afb68b727a107885b981e12
thinh <thinhwar123@gmail.com>
2026-04-30 10:58:00 +0700

    Fix Data Module

---
+++ Packages/TW.DataModule/Runtime/DataStoreAttribute.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace DataModule
+{
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = true)]
+    public class DataStoreAttribute : Attribute
+    {
+        
+    }
+}
---
+++ Packages/TW.DataModule/Runtime/IDataModule.cs
@@ -0,0 +1,14 @@
+﻿namespace DataModule
+{
+    public interface IDataModule
+    {
+        string Key { get; }
+        int Version { get; }
+
+        void SetDefault();
+        void FromJson(string json);
+        string ToJson();
+
+        void Migrate();
+    }
+}
